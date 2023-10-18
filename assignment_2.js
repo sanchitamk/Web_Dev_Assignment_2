be0741fe75050d94cffe2cb502fdf26bd07d14e3
sanchitamk <shasanch@sheridancollege.ca>
2023-10-17 22:39:02 -0400

    Creating footer cookie banner inhtml and changing it dynamically in js
    Using JavaScript and DOM manipulation, simulate the ability for a user to "accept cookie usage". Provide a footer banner where a user can click a link to "Accept Cookies." When the user clicks "Accept Cookies", change the content on the banner to display new content in the footer stating: "Cookies were accepted. Would you like to revoke?" and provide a link for the user to revoke. If the user chooses to revoke, use JavaScript to revert to the original banner content.

--- assignment_2.js
+++ assignment_2.js
@@ -68,6 +68,29 @@ for(let i=0;i<data.length;i++){
 }
 
 
+//Part 2
+
+
+let acceptance = document.querySelector("#acceptance");
+let cookietext = document.querySelector("#cookietext");
+
+
+let cookieTextNode=document.createTextNode("Accept Cookies");
+acceptance.appendChild(cookieTextNode);
+
+
+acceptance.addEventListener("click", cookieAccept);
+
+function cookieAccept(event) {
+
+    if (acceptance.innerHTML === "Accept Cookies") {
+        cookietext.innerHTML = "Cookies were accepted. Would you like to revoke?";
+        acceptance.innerHTML = "Revoke";
+    } else {
+        cookietext.innerHTML = "This website stores cookies on your computer. These cookies are used to collect information about how you interact with our website and allow us to remember you. We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors both on this website and other media. To find out more about the cookies we use, see our Privacy Policy.";
+        acceptance.innerHTML = "Accept Cookies";
+    }
+}
 
 
 
